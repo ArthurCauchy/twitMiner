999f81b5766a9d47530e68ff769909461e661f3d
ArthurCauchy <arthur.cauchy@free.fr>
2017-03-31 09:50:05 +0200

    Ajout du code de test de twitter4j

--- TwitMiner.Phase0/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase0/App.java
+++ TwitMiner.Phase0/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase0/App.java
@@ -1,13 +1,29 @@
 package Artaud.Cauchy.Fenoll.TwitMiner.Phase0;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws TwitterException
     {
-        System.out.println( "Hello World!" );
+    	// The factory instance is re-useable and thread safe.
+    	ConfigurationBuilder cb = new ConfigurationBuilder();
+    	cb.setDebugEnabled(true)
+    	  .setOAuthConsumerKey("*********************")
+    	  .setOAuthConsumerSecret("******************************************")
+    	  .setOAuthAccessToken("**************************************************")
+    	  .setOAuthAccessTokenSecret("******************************************");
+        Twitter twitter = TwitterFactory.getSingleton();
+        List<twitter4j.Status> statuses = twitter.getHomeTimeline();
+        System.out.println("Showing home timeline.");
+        for (twitter4j.Status status : statuses) {
+            System.out.println(status.getUser().getName() + ":" +
+                               status.getText());
+        }
     }
 }
