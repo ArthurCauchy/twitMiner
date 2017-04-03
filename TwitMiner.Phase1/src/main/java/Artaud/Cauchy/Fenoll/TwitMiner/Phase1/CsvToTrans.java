34e711d1abed5fb494c88762648c9a7d1e87c1f0
ARTAUD Corentin <a15001489@A-F001925-L.salsa.univ-amu.fr>
2017-04-03 16:11:44 +0200

    fin etape 1

--- TwitMiner.Phase1/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase1/CsvToTrans.java
+++ TwitMiner.Phase1/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase1/CsvToTrans.java
@@ -1,11 +1,20 @@
 package Artaud.Cauchy.Fenoll.TwitMiner.Phase1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectOutputStream;import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class CsvToTrans 
 {
@@ -16,23 +25,52 @@ public class CsvToTrans
     		System.exit(1);
     	}
     	Map<String, Integer> mapTrans = new HashMap<String, Integer>();
+    	List<String> twits = new ArrayList<String>();
     	try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
     	    String line;
     	    Integer count = 0;
     	    while ((line = br.readLine()) != null) {
-    	    	String[] words = line.split(";");
+    	    	twits.add(line);
+    	    	String[] words = line.split("\";\"");
     	    	for (int i = 2; i < words.length; ++i) {
-    	    		String word = words[i].substring(1, words[i].length()-1);
+    	    		if (words[i].length() == 0) continue;
+    	    		//System.out.println(words[i]);
+    	    		String word = words[i].substring(0, words[i].length());
     	    		if (!mapTrans.containsKey(word)) {
     	    			mapTrans.put(word, ++count);
     	    		}
     	    	}
     	    }
     	}
+    	/*
     	for (Map.Entry<String, Integer> entry : mapTrans.entrySet()) {
     	    String key = entry.getKey();
     	    Object value = entry.getValue();
     	    System.out.println(key + " ||| " + value);
     	}
+    	*/
+    	
+    	File outFile = new File(args[1]);
+    	outFile.createNewFile();
+    	BufferedWriter output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile)));
+    	for (String twit : twits) {
+    		String sentence = new String();
+    		String words [] = twit.split("\";\"");
+    		for (int i = 2; i < words.length; ++i) {
+    			String word = words[i];
+    			sentence += mapTrans.get(word.substring(0, word.length()))+" ";	
+    		}
+    		output.write(sentence);
+    		output.newLine();
+    	}
+    	output.close();
+    	
+    	Map<Integer, String> mapAssoc = mapTrans.entrySet().stream()
+    			.collect(Collectors.toMap(Entry::getValue, Entry::getKey));
+    	File mapFile = new File(args[0].split("\\.")[0] + ".map");
+    	mapFile.createNewFile();
+    	ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(mapFile));
+    	out.writeObject(mapAssoc);
+    	out.close();
     }
 }
---
+++ TwitMiner.Phase1/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase1/OutToCsv.java
@@ -0,0 +1,48 @@
+package Artaud.Cauchy.Fenoll.TwitMiner.Phase1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Map;
+
+public class OutToCsv {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+		if (args.length != 1) {
+			System.out.println("Usage : ./CsvToTrans <file.apriori>");
+		}
+		File mapFile = new File(args[0].split("\\.")[0] + ".map");
+		
+		Map<Integer, String> mapAssoc = null;
+		try(ObjectInputStream in = new ObjectInputStream(new FileInputStream(mapFile))){
+			mapAssoc = (Map<Integer, String>) in.readObject();
+		}
+		System.out.println(mapAssoc);
+		
+		File aprioriFile = new File(args[0]);
+		try(BufferedReader in = new BufferedReader(new FileReader(aprioriFile))) {
+			File aprioriCsv = new File(args[0] + ".csv");
+			aprioriCsv.createNewFile();
+			try (BufferedWriter out = new BufferedWriter(new FileWriter(aprioriCsv))){
+				String line;
+				while ((line = in.readLine()) != null) {
+					String words [] = line.split(" ");
+					for (int i = 0; i < words.length - 1; ++i) {
+						//System.out.println(mapAssoc.get(Integer.parseInt(words[i])));
+						out.write("\"" + (mapAssoc.get(Integer.parseInt(words[i])) + "\";"));
+					}
+					out.write("\"" + words[words.length - 1] + "\";");
+					out.newLine();
+				}
+				
+			}
+		}
+	}
+
+}
