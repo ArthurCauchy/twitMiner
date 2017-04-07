93985ba7585b31b8c052aa66c86f72dec7de7dd0
ArthurCauchy <arthur.cauchy@free.fr>
2017-04-07 15:32:34 +0200

    Ajout du filtrage + ajout de quelques mots

--- TwitMiner.Phase3/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase3/Filter.java
+++ TwitMiner.Phase3/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase3/Filter.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Filter {
 	public static void main(String[] args) throws IOException {
@@ -12,19 +14,30 @@ public class Filter {
 			return;
 		}
 		BufferedReader fileIn = new BufferedReader(new FileReader(args[0]));
-		PrintWriter writer = new PrintWriter(args[1], "UTF-8");
-		BufferedReader uselessDico = null;
-		if (args[2] != null) {
-			uselessDico = new BufferedReader(new FileReader(args[2]));
+		PrintWriter fileOut = new PrintWriter(args[1], "UTF-8");
+		BufferedReader uselessDicoReader = null;
+		if (args.length > 2) {
+			uselessDicoReader = new BufferedReader(new FileReader(args[2]));
 		} else {
-			uselessDico = new BufferedReader(new FileReader("motinutiles.txt"));
+			uselessDicoReader = new BufferedReader(new FileReader("motinutiles.txt"));
 		}
-		String line;
-	    while ((line = fileIn.readLine()) != null) {
-	    	String[] words = line.split("\";\"");
-	    	for (int i = 2; i < words.length; ++i) {
-	    		System.out.println(words);
+		List<String> uselessDico = new ArrayList<String>();
+		String lineDico;
+	    while ((lineDico = uselessDicoReader.readLine()) != null) {
+	    	uselessDico.add(lineDico);
+	    }
+		String lineIn;
+	    while ((lineIn = fileIn.readLine()) != null) {
+	    	System.out.println(lineIn);
+	    	String[] words = lineIn.split("\";\"");
+	    	String lineOut = "";
+	    	for (int i = 0; i < words.length; ++i) {
+	    		if (uselessDico.contains(words[i]))
+	    			continue;
+	    		lineOut += "\"" + words[i] + "\";";
 	    	}
+	    	//fileOut.println(lineOut);
+	    	System.out.println(lineOut);
 	    	System.out.println("");
 	    }
 	}
