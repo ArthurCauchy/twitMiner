2ba322313c34798c83fd277b73771faca15f215a
ARTAUD Corentin <a15001489@A-F001925-L.salsa.univ-amu.fr>
2017-04-07 16:46:50 +0200

    Fin phase2

--- TwitMiner.Phase2/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase2/ExtractAssoc.java
+++ TwitMiner.Phase2/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase2/ExtractAssoc.java
@@ -1,12 +1,17 @@
 package Artaud.Cauchy.Fenoll.TwitMiner.Phase2;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ExtractAssoc {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -14,27 +19,43 @@ public class ExtractAssoc {
 			System.out.println("Usage : <File.apriori> <minconf>");
 			return;
 		}
-		
+		double minConf = Double.parseDouble(args[1]);
 		List<String> motifs = new ArrayList<String>(); // Contiendra le fichier ligne par ligne
 		File apriori = new File(args[0]); // Le fichier d'entrée
+		File outFile = new File(args[0].split("\\.")[0]+".assoc");
 		try (BufferedReader in = new BufferedReader(new FileReader(apriori))) {
 			String line = null;
 			while ((line = in.readLine()) != null){
 				motifs.add(line); // On place chaque ligne du fichier dnans l'arraylist
 			}
 		}
-		for (String line : motifs) {
-			Motif courant = new Motif(line); // On crée un nouveau motif Y à partir de la ligne
-			//System.out.println(courant.getItems().toString() + " | " + courant.getSuport());
-			if (courant.getItems().size() < 2) continue; // Si moins de 2 occurences on passe au suivant
-			for (String line2 : motifs) {
-				Motif sub = new Motif(line2); // On crée un second motif X
-				if ((sub.getItems().isEmpty()) // Si X ne contient pas d'items
-					||(! courant.isSubEns(sub)) // OU X n'est pas un sous-ensemble de Y
-					||(sub.getItems().containsAll(courant.getItems()))) // OU si X = Y
-					continue; // On passe au suivant
-				//System.out.println("------" + sub.getItems().toString() + " | " + sub.getSuport());
-				System.out.println(sub.getItems().toString() + " | " + sub.getSuport() + " sous-ensemble de " + courant.getItems().toString() + " | " + courant.getSuport());
+		
+		try (BufferedWriter out = new BufferedWriter(new FileWriter(outFile))) {
+			for (String line : motifs) {
+				Motif courant = new Motif(line); // On crée un nouveau motif Y à partir de la ligne
+				if (courant.getItems().size() < 2) continue; // Si moins de 2 occurences on passe au suivant
+				for (String line2 : motifs) {
+					Motif sub = new Motif(line2); // On crée un second motif X
+					if ((sub.getItems().isEmpty()) // Si X ne contient pas d'items
+						||(! sub.estSousEnsembleDe(courant)) // OU X n'est pas un sous-ensemble de Y
+						||(sub.getItems().equals(courant.getItems()))) // OU si X = Y
+						continue; // On passe au suivant
+					//System.out.println(""+courant.getSuport()+" / "+sub.getSuport());
+					double conf = ((double)courant.getSuport())/((double)sub.getSuport());
+					if (conf > 1) continue;	// suprimer les confiances superieur à 1 dù à un probleme d'apriori
+					if (conf > minConf) {
+						String strCour = new String();
+						Set<Integer> setCour =  new HashSet<Integer>(courant.getItems());
+						setCour.removeAll(sub.getItems());
+						for (Integer elem : setCour) {
+							strCour += ""+elem+" ";
+						}
+						out.write(sub.toString().substring(0, sub.toString().length()-1)+
+								" -> "+strCour+"("+conf+")");
+						out.newLine();
+						out.flush();
+					}
+				}
 			}
 		}
 	}
--- TwitMiner.Phase2/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase2/Motif.java
+++ TwitMiner.Phase2/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase2/Motif.java
@@ -1,16 +1,16 @@
 package Artaud.Cauchy.Fenoll.TwitMiner.Phase2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Motif {
 	private int suport;
-	private List<Integer> items;
+	private Set<Integer> items;
 	
 	public Motif(String values) {
 		String itemsS [] = values.split(" "); // On sépare en plusieurs chaînes au niveau des espaces
 		suport = Integer.parseInt(itemsS[itemsS.length-1].substring(1, itemsS[itemsS.length-1].length()-1)); // Le nombre d'occurences
-		items = new ArrayList<Integer>();
+		items = new HashSet<Integer>();
 		for (int i  = 0; i < itemsS.length-1; ++i) {
 			//System.out.println("---" + Integer.parseInt(itemsS[i]));
 			items.add(Integer.parseInt(itemsS[i])); // On ajoute les items
@@ -21,12 +21,22 @@ public class Motif {
 		return suport;
 	}
 	
-	public List<Integer> getItems() {
+	public Set<Integer> getItems() {
 		return items;
 	}
 	
 	/* true si c'est un sous-ensemble */
-	public boolean isSubEns (Motif motif) {
-		return items.containsAll(motif.getItems());
+	public boolean estSousEnsembleDe (Motif motif) {
+		return motif.items.containsAll(this.items);
+	}
+	
+	@Override
+	public String toString() {
+		String res = new String();
+		for (Integer elem : items) {
+			res += elem+" ";
+		}
+		return res;
 	}
 }
+
