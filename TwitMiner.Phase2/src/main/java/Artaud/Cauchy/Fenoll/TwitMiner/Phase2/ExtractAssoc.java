ad7c6d708d5e881204755968392864ceee5c8cea
ArthurCauchy <arthur.cauchy@free.fr>
2017-04-19 21:40:24 +0200

    Removed unused imports

--- TwitMiner.Phase2/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase2/ExtractAssoc.java
+++ TwitMiner.Phase2/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase2/ExtractAssoc.java
@@ -7,7 +7,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.StringCharacterIterator;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
