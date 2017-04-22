08eefce13e63e9b101f546a2efedc180c5461e68
ArthurCauchy <arthur.cauchy@free.fr>
2017-04-22 15:34:59 +0200

    CSV to trans now generate a dictionnary (map), and an output (trans)

--- TwitMiner.Phase1/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase1/CsvToTrans.java
+++ TwitMiner.Phase1/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase1/CsvToTrans.java
@@ -1,76 +1,54 @@
 package Artaud.Cauchy.Fenoll.TwitMiner.Phase1;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 public class CsvToTrans 
 {
-    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException
+    public static void main(String[] args) throws IOException
     {
-    	if (args.length != 2) {
-    		System.out.println("Usage : ./CsvToTrans <file.csv> <file.map>");
+    	if (args.length != 3) {
+    		System.out.println("Usage : ./CsvToTrans <file.csv> <file.map> <file.trans>");
     		return;
     	}
+    	
     	Map<String, Integer> mapTrans = new HashMap<String, Integer>();
-    	List<String> twits = new ArrayList<String>();
-    	try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-    	    String line;
-    	    Integer count = 0;
-    	    while ((line = br.readLine()) != null) {
-    	    	twits.add(line);
-    	    	String[] words = line.split("\";\"");
-    	    	for (int i = 2; i < words.length; ++i) {
-    	    		if (words[i].length() == 0) continue;
-    	    		//System.out.println(words[i]);
-    	    		String word = words[i].substring(0, words[i].length());
-    	    		if (!mapTrans.containsKey(word)) {
-    	    			mapTrans.put(word, ++count);
-    	    		}
-    	    	}
-    	    }
-    	}
-    	/*
-    	for (Map.Entry<String, Integer> entry : mapTrans.entrySet()) {
-    	    String key = entry.getKey();
-    	    Object value = entry.getValue();
-    	    System.out.println(key + " ||| " + value);
-    	}
-    	*/
+		BufferedReader in = new BufferedReader(new FileReader(args[0]));
+		PrintWriter out = new PrintWriter(args[2], "UTF-8");
+	    
+		String line;
+	    Integer count = 0;
+	    while ((line = in.readLine()) != null) {
+	    	String[] words = line.split("\";\"");
+	    	for (int i = 2; i < words.length; ++i) {
+	    		if (words[i].length() == 0) continue;
+	    		String word = words[i].substring(0, words[i].length());
+	    		Integer wordID = mapTrans.get(word);
+	    		if (wordID == null) {
+	    			out.print(count+1);
+	    			mapTrans.put(word, ++count);
+	    		}
+	    		else
+	    			out.print(wordID);
+	    		if (i < words.length -1)
+	    			out.print(' ');
+	    	}
+	    	out.print('\n');
+	    }
+	    
+	    in.close();
+	    out.close();
     	
-    	File outFile = new File(args[1]);
-    	outFile.createNewFile();
-    	BufferedWriter output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile)));
-    	for (String twit : twits) {
-    		String sentence = new String();
-    		String words [] = twit.split("\";\"");
-    		for (int i = 2; i < words.length; ++i) {
-    			String word = words[i];
-    			sentence += mapTrans.get(word.substring(0, word.length()))+" ";	
-    		}
-    		output.write(sentence);
-    		output.newLine();
-    	}
-    	output.close();
+	    ObjectOutputStream dicoOut = new ObjectOutputStream(new FileOutputStream(args[1]));
+	    
+    	dicoOut.writeObject(mapTrans); // On serialize la relation entre les mots et leurs nombre pour plus tard
     	
-    	Map<Integer, String> mapAssoc = mapTrans.entrySet().stream()
-    			.collect(Collectors.toMap(Entry::getValue, Entry::getKey));
-    	File mapFile = new File(args[0].split("\\.")[0] + ".map");
-    	mapFile.createNewFile();
-    	ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(mapFile));
-    	out.writeObject(mapAssoc);
-    	out.close();
+    	dicoOut.close();
     }
 }
