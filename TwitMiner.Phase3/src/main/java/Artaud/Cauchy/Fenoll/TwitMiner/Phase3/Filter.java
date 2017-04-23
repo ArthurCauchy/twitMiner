945d301b5da83763cbf4b0f2f0f4d665f8084e7e
ArthurCauchy <arthur.cauchy@free.fr>
2017-04-23 15:05:21 +0200

    Déplacement du fichier annexe motinutiles.txt

--- TwitMiner.Phase3/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase3/Filter.java
+++ TwitMiner.Phase3/src/main/java/Artaud/Cauchy/Fenoll/TwitMiner/Phase3/Filter.java
@@ -3,6 +3,7 @@ package Artaud.Cauchy.Fenoll.TwitMiner.Phase3;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +20,7 @@ public class Filter {
 		if (args.length > 2) {
 			uselessDicoReader = new BufferedReader(new FileReader(args[2]));
 		} else {
-			uselessDicoReader = new BufferedReader(new FileReader("motinutiles.txt"));
+			uselessDicoReader = new BufferedReader(new InputStreamReader(ClassLoader.class.getResourceAsStream("/res/motinutiles.txt")));
 		}
 		List<String> uselessDico = new ArrayList<String>();
 		String lineDico;
